437843a5d6e14e30e5ba1cc872e56e75ddb7d3cf
KaumudiMummadi <kaumudi.mummadi@gmail.com>
2018-07-23 20:30:24 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -3,3 +3,5 @@ party_name = "Best Halloween Party Ever"
 date = "October 31"
 time = "6pm"
 host_name= "Harry Potter"
+
+puts "Dear #{guest_name}, You are cordially invited to the #{party_name} on #{date} at #{time}.Please RSVP no later than October 30. Sincerely, #{host_name}."
